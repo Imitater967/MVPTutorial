d79574227f2765e9fd1a804e0c215911dcf61226
Imitater967 <imitater967@qq.com>
2024-04-18 17:01:19 +0800

    完成基本的

--- Assets/Scripts/UI/InventoryItemPresenter.cs
+++ Assets/Scripts/UI/InventoryItemPresenter.cs
@@ -1,8 +1,44 @@
-using Git.Imitater967.MVPTutorial.UI;
+using System;
+using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
 
 namespace Git.Imitater967.MVPTutorial.UI {
     public class InventoryItemPresenter : AbstractPresenter<AbstractInventoryModel,InventoryView> {
+        [SerializeField]
         protected ItemPresenter m_ItemPrefab;
+        protected Dictionary<AbstractItemModel, ItemPresenter> m_ModelDict;
+
+        private void Awake() {
+            m_ModelDict = new Dictionary<AbstractItemModel, ItemPresenter>();
+        }
+
+        private void Start() {
+            m_Model.Items.ObserveRemove().Subscribe(evt => {
+                DestroyItemPresenter(evt.Value);
+            });
+            m_Model.Items.ObserveReset().Subscribe(evt => {
+                foreach (var modelDictValue in new List<AbstractItemModel>(m_ModelDict.Keys)) {
+                    DestroyItemPresenter(modelDictValue);
+                }
+            });
+            m_Model.Items.ObserveAdd().Subscribe(evt => {
+                InstantiateItemPresenter(evt.Value);
+            });
+            for (var i = 0; i < m_Model.Items.Count; i++) {
+                InstantiateItemPresenter(m_Model.Items[i]);
+            }
+        }
+
+        private void DestroyItemPresenter(AbstractItemModel model) {
+            Destroy(m_ModelDict[model].gameObject);
+            m_ModelDict.Remove(model);
+        }
+
+        public void InstantiateItemPresenter(AbstractItemModel model) {
+            var itemPresenter = Instantiate(m_ItemPrefab, m_View.ItemParent);
+            itemPresenter.Initialize(model);
+            m_ModelDict.Add(model,itemPresenter);
+        }
     }
 }
--- Assets/Scripts/UI/ItemPresenter.cs
+++ Assets/Scripts/UI/ItemPresenter.cs
@@ -1,13 +1,23 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Git.Imitater967.MVPTutorial.UI {
 
     [Serializable]
     public class ItemView {
+        [field: SerializeField]
+        public Image m_Icon;
+
+        public void SetIconImage(Sprite sprite) {
+            m_Icon.sprite = sprite;
+        }
     }
 
     public class ItemPresenter : AbstractPresenter<AbstractItemModel,ItemView> {
-        
+        public void Initialize(AbstractItemModel modelItem) {
+            m_Model = modelItem;
+            m_View.SetIconImage(m_Model.Sprite);
+        }
     }
 }
